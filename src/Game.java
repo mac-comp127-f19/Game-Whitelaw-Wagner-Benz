08c2821d4a9316efacd557133e2f91a865e0802b
jawhitelaw <jwhitela@macalester.edu>
2019-12-06 13:55:48 -0600

    ...

--- src/Game.java
+++ src/Game.java
@@ -51,8 +51,8 @@ public class Game {
             ball5.moveBallLinear(.1, bound);
         });
         canvas.onDrag((mouseMotionEvent) -> {
-            if (mouseMotionEvent.getPosition().getX()-player.getCenter().getX()<10&&mouseMotionEvent.getPosition().getX()-player.getCenter().getX()>-10
-                    &&mouseMotionEvent.getPosition().getY()-player.getCenter().getY()<10&&mouseMotionEvent.getPosition().getY()-player.getCenter().getY()>-10)
+            if (mouseMotionEvent.getPosition().getX()-player.getCenter().getX()<20&&mouseMotionEvent.getPosition().getX()-player.getCenter().getX()>-20
+                    &&mouseMotionEvent.getPosition().getY()-player.getCenter().getY()<20&&mouseMotionEvent.getPosition().getY()-player.getCenter().getY()>-20)
                 player.setCenter(mouseMotionEvent.getPosition());
         });
     }
