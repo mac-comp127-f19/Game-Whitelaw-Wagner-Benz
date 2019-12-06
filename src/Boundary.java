fee1615a7847ddc6aeb5e54c43edb3488ad6caf1
tybenz12 <tbenz1@macalester.edu>
2019-12-06 13:28:23 -0600

    Collisions working again

--- src/Boundary.java
+++ src/Boundary.java
@@ -48,82 +48,66 @@ public class Boundary{
         line1 = new Line(40,200,40,600);
         line1.setStrokeWidth(7.5);
         group.add(line1);
-        bounds.add(line1);
 
         line2 = new Line(860,200,860,600);
         line2.setStrokeWidth(7.5);
         group.add(line2);
-        bounds.add(line2);
 
         line3 = new Line(40,200,200,200);
         line3.setStrokeWidth(7.5);
         group.add(line3);
-        bounds.add(line3);
 
         line4 = new Line(600,200,860,200);
         line4.setStrokeWidth(7.5);
         group.add(line4);
-        bounds.add(line4);
 
         line5 = new Line(40,600,300,600);
         line5.setStrokeWidth(7.5);
         group.add(line5);
-        bounds.add(line5);
 
         line6 = new Line(700,600,860,600);
         line6.setStrokeWidth(7.5);
         group.add(line6);
-        bounds.add(line6);
 
         line7 = new Line(700,250,700,600);
         line7.setStrokeWidth(7.5);
         group.add(line7);
-        bounds.add(line7);
 
         line8 = new Line(650,250,700,250);
         line8.setStrokeWidth(7.5);
         group.add(line8);
-        bounds.add(line8);
 
         line9 = new Line(200,200,200,550);
         line9.setStrokeWidth(7.5);
         group.add(line9);
-        bounds.add(line9);
 
         line10 = new Line(200,550,250,550);
         line10.setStrokeWidth(7.5);
         group.add(line10);
-        bounds.add(line10);
 
         line11 = new Line(250,250,250,550);
         line11.setStrokeWidth(7.5);
         group.add(line11);
-        bounds.add(line11);
 
         line12 = new Line(650,250,650,550);
         line12.setStrokeWidth(7.5);
         group.add(line12);
-        bounds.add(line12);
 
         line13 = new Line(250,250,600,250);
         line13.setStrokeWidth(7.5);
         group.add(line13);
-        bounds.add(line13);
 
         line14 = new Line(300,550,650,550);
         line14.setStrokeWidth(7.5);
         group.add(line14);
-        bounds.add(line14);
 
         line15 = new Line(600,200,600,250);
         line15.setStrokeWidth(7.5);
         group.add(line15);
-        bounds.add(line15);
 
         line16 = new Line(300,550,300,600);
         line16.setStrokeWidth(7.5);
         group.add(line16);
-        bounds.add(line16);
 
         startRectangle = new Rectangle(43.75,203.75,152.5,392.5);
         startRectangle.setFillColor(Color.GREEN);
@@ -137,6 +121,7 @@ public class Boundary{
         middleRectangle.setFillColor(Color.gray);
         middleRectangle.setStrokeColor(Color.gray);
         group.add(middleRectangle);
+        bounds.add(middleRectangle);
 
         leftFillRectangle = new Rectangle(196.5,553.75,57.5,50-8);
         leftFillRectangle.setFillColor(Color.gray);
