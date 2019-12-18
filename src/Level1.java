134a706f864e40d8c3c09008e1fe703069f93e35
jawhitelaw <jwhitela@macalester.edu>
2019-12-17 23:44:04 -0600

    level

--- src/Level1.java
+++ src/Level1.java
@@ -1,11 +1,10 @@
 import comp127graphics.CanvasWindow;
 import comp127graphics.GraphicsText;
-import comp127graphics.events.Key;
 
 import java.awt.*;
 import java.util.ArrayList;
 
-public class Level1 implements Level{
+public class Level1 implements Level {
 
     private CanvasWindow canvas;
     private Ball ball1;
@@ -19,6 +18,10 @@ public class Level1 implements Level{
     private Boundary bound;
     private ArrayList<Ball> balls;
     private boolean boundHit;
+    private GraphicsText level;
+    private GraphicsText deaths;
+    private int deathCounter = 0;
+    private GraphicsText win;
 
     /**
      * Creates a new Game object
@@ -27,22 +30,21 @@ public class Level1 implements Level{
         canvas = new CanvasWindow("", 900, 900);
         bound = new Boundary(canvas);
         bound.addBoundary();
-        ball1 = new Ball(450, 275, 175, 0, 25, this.canvas);
+        ball1 = new Ball(450, 275, 100, 0, 25, this.canvas);
         ball1.addBall();
-        ball2 = new Ball(450, 375, 175, 0, 25, this.canvas);
+        ball2 = new Ball(450, 375, 100, 0, 25, this.canvas);
         ball2.addBall();
-        ball3 = new Ball(450, 475, 175, 0, 25, this.canvas);
+        ball3 = new Ball(450, 475, 100, 0, 25, this.canvas);
         ball3.addBall();
-        ball4 = new Ball(450, 325, -175, 0, 25, this.canvas);
+        ball4 = new Ball(450, 325, -100, 0, 25, this.canvas);
         ball4.addBall();
-        ball5 = new Ball(450, 425, -175, 0, 25, this.canvas);
+        ball5 = new Ball(450, 425, -100, 0, 25, this.canvas);
         ball5.addBall();
-        ball6 = new Ball(450, 525, -175, 0, 25, this.canvas);
+        ball6 = new Ball(450, 525, -100, 0, 25, this.canvas);
         ball6.addBall();
         player = new Player(125, 400, 25, canvas);
         player.addPlayer();
         canvas.setBackground(new Color(131, 143, 255));
-        canvas.draw();
         balls = new ArrayList<>();
         balls.add(ball1);
         balls.add(ball2);
@@ -52,8 +54,16 @@ public class Level1 implements Level{
         balls.add(ball6);
         boundHit = player.testBoundaryHit(bound);
         instructions = new GraphicsText("Get to the other green box to complete the level");
+        level = new GraphicsText("Level: 1");
+        deaths = new GraphicsText("Deaths: " + deathCounter);
         instructions.setFontSize(20);
-        canvas.add(instructions, 50, 50);
+        level.setFontSize(50);
+        deaths.setFontSize(50);
+        canvas.add(level, 5, 45);
+        canvas.add(instructions, 100, 100);
+        canvas.add(deaths, 600, 45);
+        win = new GraphicsText("You Win");
+        win.setFontSize(200);
     }
 
     public void run() {
@@ -63,6 +73,8 @@ public class Level1 implements Level{
                 ball.moveBallLinear(.1, bound);
                 if (ball.testPlayerHit(player)) {
                     player.setCenter(125, 400);
+                    deathCounter++;
+                    deaths.setText("Deaths: " + deathCounter);
                 }
             }
         });
@@ -72,6 +84,10 @@ public class Level1 implements Level{
             if (test1 && test2 && !boundHit && bound.getGroup().testHit(mouseMotionEvent.getPosition().getX(), mouseMotionEvent.getPosition().getY())) {
                 player.setCenter(mouseMotionEvent.getPosition());
             }
+            if (bound.getEndRectangle().testHit(player.getCenter().getX(), player.getCenter().getY())){
+                canvas.add(win, 75, 500);
+            }
         });
+
     }
 }
